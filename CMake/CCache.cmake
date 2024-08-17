33531511d8e5db816be73118f06105519c7f6f05
Laggamer2005 <158077584+Laggamer2005@users.noreply.github.com>
2024-08-17 20:51:09 +0100

    Update CCache.cmake

--- CMake/CCache.cmake
+++ CMake/CCache.cmake
@@ -1,10 +1,11 @@
 find_program(CCACHE_BIN NAMES ccache sccache)
+
 if(CCACHE_BIN)
     set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE ${CCACHE_BIN})
 
-    # ccache uses -I when compiling without preprocessor, which makes clang complain.
+    # Handle Clang specific flags
     if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
-        set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Qunused-arguments -fcolor-diagnostics")
-        set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Qunused-arguments -fcolor-diagnostics")
+        list(APPEND CMAKE_CXX_FLAGS -Qunused-arguments -fcolor-diagnostics)
+        list(APPEND CMAKE_C_FLAGS -Qunused-arguments -fcolor-diagnostics)
     endif()
 endif()
